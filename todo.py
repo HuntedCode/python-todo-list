4f399679c1d7132e60d23db180582cc48a077f8e
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-07-19 11:41:11 -0400

    Added docstrings

--- todo.py
+++ todo.py
@@ -2,27 +2,37 @@ todo = ["Practice Python", "Learn Arabic", "Get groceries"]
 accepted_commands = {"add", "edit", "exit", "help", "move", "remove", "view"}
 
 def display_list(list):
+    """Displays list param in command line."""
+
     print("\nHello! Here is your current to-do list:")
 
     for index, value in enumerate(list):
         print(index + 1, value)
 
 def add_command(list):
+    """Appends a user input item to list param."""
+
     item = input("What would you like to add?: ")
     list.append(item)
     print("Item added successfully!")
 
 def edit_command(list):
+    """Updates user specificed list index with new user input item."""
+
     index = get_input_index("Which item number would you like to edit?: ", list)
     new_item = input("What would you like to change #{0} to?: ".format(index + 1))
     list[index] = new_item
     print("Item updated successfully!")
 
 def help_command():
+    """Displays list of valid commands on command line."""
+
     print("Use the following commands to manipulate your list:")
     print(sorted(accepted_commands))
 
 def move_command(list):
+    """Moves user specified index item to new user specified index."""
+
     index_to_move = get_input_index("Which item number would you like to move?: ", list)
     index_to_replace = get_input_index("Where would you like to place it in the list?: ", list)
 
@@ -34,12 +44,16 @@ def move_command(list):
   
 
 def remove_command(list):
+    """Removes item at user specified index."""
+
     index = get_input_index("Which item number would you like to remove?: ", list)
     list.pop(index)
     print("Item removed successfully!")
 
 
 def get_input_command():
+    """Validates and returns user inputted command."""
+
     while(True):
         command = input("What would you like to do?: ")
 
@@ -49,6 +63,8 @@ def get_input_command():
             print("Please enter a valid command. Type 'help' for list of commands.\n")
 
 def get_input_index(input_text, list):
+    """Returns and validates user inputted index."""
+
     while(True):
         try:
             user_input = int(input(input_text))
@@ -62,6 +78,8 @@ def get_input_index(input_text, list):
             print("Your list does not contain that item number!\n")
 
 def process_command(command, list):
+    """Takes param command and calls appropriate proccessing function."""
+    
     match command:
         case "add":
             add_command(list)
