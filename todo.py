087f8a48a9d12e0899c3a75c59ee1f62efbd6b6a
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-07-19 09:59:38 -0400

    Initial commit: Add basic to-do list script

---
+++ todo.py
@@ -0,0 +1,90 @@
+todo = ["Practice Python", "Learn Arabic", "Get groceries"]
+accepted_commands = {"add", "edit", "exit", "help", "move", "remove", "view"}
+
+def display_list(list):
+    print("\nHello! Here is your current to-do list:")
+
+    for index, value in enumerate(list):
+        print(index + 1, value)
+
+def add_command(list):
+    item = input("What would you like to add?: ")
+    list.append(item)
+    print("Item added successfully!")
+
+def edit_command(list):
+    index = get_input_index("Which item number would you like to edit?: ", list)
+    new_item = input("What would you like to change #{0} to?: ".format(index + 1))
+    list[index] = new_item
+    print("Item updated successfully!")
+
+def help_command():
+    print("Use the following commands to manipulate your list:")
+    print(sorted(accepted_commands))
+
+def move_command(list):
+    index_to_move = get_input_index("Which item number would you like to move?: ", list)
+    index_to_replace = get_input_index("Where would you like to place it in the list?: ", list)
+
+    item = list[index_to_move]
+    list.pop(index_to_move)  
+    list.insert(index_to_replace, item)
+
+    print("Item moved successfully!")
+  
+
+def remove_command(list):
+    index = get_input_index("Which item number would you like to remove?: ", list)
+    list.pop(index)
+    print("Item removed successfully!")
+
+
+def get_input_command():
+    while(True):
+        command = input("What would you like to do?: ")
+
+        if command in accepted_commands:
+            return command
+        else:
+            print("Please enter a valid command. Type 'help' for list of commands.\n")
+
+def get_input_index(input_text, list):
+    while(True):
+        try:
+            user_input = int(input(input_text))
+        except ValueError:
+            print("Please enter an integer!\n")
+        
+        index = user_input - 1
+        if index in range(0, len(list)):
+            return index
+        else:
+            print("Your list does not contain that item number!\n")
+
+def process_command(command, list):
+    match command:
+        case "add":
+            add_command(list)
+        case "edit":
+            edit_command(list)
+        case "help":
+            help_command()
+        case "move":
+            move_command(list)
+        case "remove":
+            remove_command(list)
+        case "view":
+            display_list(list)
+
+display_list(todo)
+
+while(True):
+    print()
+
+    command = get_input_command()
+
+    if command == "exit":
+        break
+
+    process_command(command, todo)
+
