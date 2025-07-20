856319a93a6a06eda536cffafb674120e4d2d268
Jeffrey Lowe <HuntedCode47@gmail.com>
2025-07-20 12:49:26 -0400

    Add basic file i/o and error handling.

--- todo.py
+++ todo.py
@@ -1,8 +1,9 @@
 todo = ["Practice Python", "Learn Arabic", "Get groceries"]
-accepted_commands = {"add", "edit", "exit", "help", "move", "remove", "view"}
+accepted_commands = {"add", "clear", "edit", "exit", "filename", "help", "load", "move", "remove", "save", "view"}
+filename = None
 
 def display_list(list):
-    """Displays list param in command line."""
+    """Displays list arg in command line."""
 
     print("\nHello! Here is your current to-do list:")
 
@@ -10,17 +11,22 @@ def display_list(list):
         print(index + 1, value)
 
 def add_command(list):
-    """Appends a user input item to list param."""
+    """Appends a user input item to list arg."""
 
     item = input("What would you like to add?: ")
     list.append(item)
     print("Item added successfully!")
 
+def clear_command(list):
+    """Clears list arg."""
+
+    list.clear()
+
 def edit_command(list):
     """Updates user specificed list index with new user input item."""
 
     index = get_input_index("Which item number would you like to edit?: ", list)
-    new_item = input("What would you like to change #{0} to?: ".format(index + 1))
+    new_item = input(f"What would you like to change #{index + 1} to?: ")
     list[index] = new_item
     print("Item updated successfully!")
 
@@ -29,6 +35,31 @@ def help_command():
 
     print("Use the following commands to manipulate your list:")
     print(sorted(accepted_commands))
+
+def load_command(list):
+    """Loads saved todo list based on user input filename."""
+
+    global filename
+
+    while(True):
+        try:
+            filename = input("Which file would you like to load?: ") + ".txt"
+
+            with open(filename, "r", encoding="utf-8") as f:
+                list.clear()
+                
+                for line in f:
+                    list.append(line.rstrip())
+        
+            if (f.closed):
+                print("File loaded successfully!")
+                break
+            else:
+                print("File did not close correctly!")
+                break
+
+        except FileNotFoundError:
+            print("Could not find your file, please try again.")
 
 def move_command(list):
     """Moves user specified index item to new user specified index."""
@@ -42,7 +73,6 @@ def move_command(list):
 
     print("Item moved successfully!")
   
-
 def remove_command(list):
     """Removes item at user specified index."""
 
@@ -50,6 +80,21 @@ def remove_command(list):
     list.pop(index)
     print("Item removed successfully!")
 
+def save_command(list):
+    """Saves todo list to user specified file."""
+
+    global filename
+    if filename is None:
+        filename = input("Enter name of the file you'd like to save: ") + ".txt"
+    
+    with open(filename, "w", encoding="utf-8") as f:
+        for item in list:
+            f.write(item + "\n")
+
+    if (f.closed):
+        print ("File saved successfully!")
+    else:
+        print("The file did not close correctly!")
 
 def get_input_command():
     """Validates and returns user inputted command."""
@@ -78,19 +123,27 @@ def get_input_index(input_text, list):
             print("Your list does not contain that item number!\n")
 
 def process_command(command, list):
-    """Takes param command and calls appropriate proccessing function."""
+    """Takes arg command and calls appropriate proccessing function."""
     
     match command:
         case "add":
             add_command(list)
+        case "clear":
+            clear_command(list)
         case "edit":
             edit_command(list)
+        case "filename":
+            print(filename)
         case "help":
             help_command()
+        case "load":
+            load_command(list)
         case "move":
             move_command(list)
         case "remove":
             remove_command(list)
+        case "save":
+            save_command(list)
         case "view":
             display_list(list)
 
